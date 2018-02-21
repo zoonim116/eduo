bb596462c03c65e54fd36d967857bbd77bb132df
Maxim Nepomniashiy <maximko91@gmail.com>
2018-02-21 11:13:40 +0200

    Setup

--- src/dependencies.php
+++ src/dependencies.php
@@ -1,13 +1,12 @@
 <?php
+use Medoo\Medoo;
+use Slim\Views\Twig;
+use \App\Src\Controllers;
+
 // DIC configuration
 
 $container = $app->getContainer();
 
-// view renderer
-$container['renderer'] = function ($c) {
-    $settings = $c->get('settings')['renderer'];
-    return new Slim\Views\PhpRenderer($settings['template_path']);
-};
 
 // monolog
 $container['logger'] = function ($c) {
@@ -17,3 +16,30 @@ $container['logger'] = function ($c) {
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
 };
+
+// medoo
+$container['db'] = function () {
+    return new Medoo([
+        'database_type' => 'mysql',
+        'database_name' => 'l_dashboard',
+        'server' => 'localhost',
+        'username' => 'root',
+        'password' => 'root'
+    ]);
+};
+
+$container['view'] = function($container) {
+    $settings = $container->get('settings')['renderer'];
+    $view = new \Slim\Views\Twig($settings['template_path'], compact('$settings["cache"]'));
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $container['request']->getUri()));
+    $view->addExtension(new Knlv\Slim\Views\TwigMessages(
+        new Slim\Flash\Messages()
+    ));
+    return $view;
+};
+
+$container['flash'] = function () {
+    return new \Slim\Flash\Messages();
+};
+
+$container['upload_directory'] = __DIR__ . '/../public/uploads';
