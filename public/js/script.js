ef69bbf00a019f920c3a759d0637b663708d3739
Maxim Nepomniashiy <maximko91@gmail.com>
2018-03-07 12:47:19 +0200

    Medium editor plugins

--- public/js/script.js
+++ public/js/script.js
@@ -8,16 +8,25 @@ $(document).ready(function () {
     });
 
     if ($('.medium-text').length > 0) {
+
         var editor = new MediumEditor('.medium-text', {
             toolbar: {
-                buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3', 'quote', 'table']
+                buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3', 'quote', 'table', 'list-extension', 'unorderedlist','orderedlist']
             },
             buttonLabels: 'fontawesome',
             extensions: {
                 markdown: new MeMarkdown(function (md) {
                     // markDownEl.textContent = md;
                 }),
-                table: new MediumEditorTable()
+                table: new MediumEditorTable(),
+                'list-extension': new MediumEditorList(),
+                autolist: new AutoList()
+            },
+            mediumEditorList: {
+                newParagraphTemplate: '<li>...</li>',
+                buttonTemplate: '<b><i class="fa fa-list" aria-hidden="true"></i></b>',
+                addParagraphTemplate: 'Add new item',
+                isEditable: true
             }
         });
         $('.medium-text').mediumInsert({
